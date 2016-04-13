bd1c7fcabf9a1e98636562d7a44ec9c0dae6c5ad
TheeMrshix <mrshix1996@gmail.com>
2016-04-13 01:02:56 +0100

    Initial Commit

---
+++ SQFEncrypt/SQFEncrypt/Main.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace SQFEncrypt
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.label1 = new System.Windows.Forms.Label();
+            this.FileInputBox = new System.Windows.Forms.TextBox();
+            this.BrowseBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.EncryptNumBox = new System.Windows.Forms.TextBox();
+            this.PvarNameBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.outputBox = new System.Windows.Forms.RichTextBox();
+            this.GenPvarBtn = new System.Windows.Forms.Button();
+            this.EncryptBtn = new System.Windows.Forms.Button();
+            this.GenToStringBtn = new System.Windows.Forms.Button();
+            this.CopyToClipboardCheck = new System.Windows.Forms.CheckBox();
+            this.stripComments = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input File:";
+            // 
+            // FileInputBox
+            // 
+            this.FileInputBox.Location = new System.Drawing.Point(13, 30);
+            this.FileInputBox.Name = "FileInputBox";
+            this.FileInputBox.Size = new System.Drawing.Size(405, 20);
+            this.FileInputBox.TabIndex = 1;
+            this.FileInputBox.TextChanged += new System.EventHandler(this.FileInputBox_TextChanged);
+            // 
+            // BrowseBtn
+            // 
+            this.BrowseBtn.Location = new System.Drawing.Point(425, 30);
+            this.BrowseBtn.Name = "BrowseBtn";
+            this.BrowseBtn.Size = new System.Drawing.Size(90, 22);
+            this.BrowseBtn.TabIndex = 2;
+            this.BrowseBtn.Text = "Browse";
+            this.BrowseBtn.UseVisualStyleBackColor = true;
+            this.BrowseBtn.Click += new System.EventHandler(this.BrowseBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Encrypt Number:";
+            // 
+            // EncryptNumBox
+            // 
+            this.EncryptNumBox.Location = new System.Drawing.Point(12, 69);
+            this.EncryptNumBox.Name = "EncryptNumBox";
+            this.EncryptNumBox.Size = new System.Drawing.Size(86, 20);
+            this.EncryptNumBox.TabIndex = 4;
+            this.EncryptNumBox.TextChanged += new System.EventHandler(this.EncryptNumBox_TextChanged);
+            // 
+            // PvarNameBox
+            // 
+            this.PvarNameBox.Location = new System.Drawing.Point(123, 69);
+            this.PvarNameBox.Name = "PvarNameBox";
+            this.PvarNameBox.Size = new System.Drawing.Size(392, 20);
+            this.PvarNameBox.TabIndex = 5;
+            this.PvarNameBox.TextChanged += new System.EventHandler(this.PvarNameBox_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(120, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Public Variable Name:";
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(12, 95);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(503, 183);
+            this.outputBox.TabIndex = 7;
+            this.outputBox.Text = "";
+            // 
+            // GenPvarBtn
+            // 
+            this.GenPvarBtn.Location = new System.Drawing.Point(12, 285);
+            this.GenPvarBtn.Name = "GenPvarBtn";
+            this.GenPvarBtn.Size = new System.Drawing.Size(99, 51);
+            this.GenPvarBtn.TabIndex = 8;
+            this.GenPvarBtn.Text = "Generate Public Variable Code";
+            this.GenPvarBtn.UseVisualStyleBackColor = true;
+            this.GenPvarBtn.Click += new System.EventHandler(this.GeneratePVarbtn_Click);
+            // 
+            // EncryptBtn
+            // 
+            this.EncryptBtn.Location = new System.Drawing.Point(416, 285);
+            this.EncryptBtn.Name = "EncryptBtn";
+            this.EncryptBtn.Size = new System.Drawing.Size(99, 51);
+            this.EncryptBtn.TabIndex = 9;
+            this.EncryptBtn.Text = "Encrypt";
+            this.EncryptBtn.UseVisualStyleBackColor = true;
+            this.EncryptBtn.Click += new System.EventHandler(this.EncryptBtn_Click);
+            // 
+            // GenToStringBtn
+            // 
+            this.GenToStringBtn.Location = new System.Drawing.Point(117, 284);
+            this.GenToStringBtn.Name = "GenToStringBtn";
+            this.GenToStringBtn.Size = new System.Drawing.Size(99, 51);
+            this.GenToStringBtn.TabIndex = 10;
+            this.GenToStringBtn.Text = "Generate toString statement";
+            this.GenToStringBtn.UseVisualStyleBackColor = true;
+            this.GenToStringBtn.Click += new System.EventHandler(this.GenToStringBtn_Click);
+            // 
+            // CopyToClipboardCheck
+            // 
+            this.CopyToClipboardCheck.AutoSize = true;
+            this.CopyToClipboardCheck.Location = new System.Drawing.Point(241, 285);
+            this.CopyToClipboardCheck.Name = "CopyToClipboardCheck";
+            this.CopyToClipboardCheck.Size = new System.Drawing.Size(169, 17);
+            this.CopyToClipboardCheck.TabIndex = 11;
+            this.CopyToClipboardCheck.Text = "Copy To Clipboard On Encrypt";
+            this.CopyToClipboardCheck.UseVisualStyleBackColor = true;
+            // 
+            // stripComments
+            // 
+            this.stripComments.AutoSize = true;
+            this.stripComments.Location = new System.Drawing.Point(241, 302);
+            this.stripComments.Name = "stripComments";
+            this.stripComments.Size = new System.Drawing.Size(153, 17);
+            this.stripComments.TabIndex = 12;
+            this.stripComments.Text = "Strip Comments From Code";
+            this.stripComments.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(527, 348);
+            this.Controls.Add(this.stripComments);
+            this.Controls.Add(this.CopyToClipboardCheck);
+            this.Controls.Add(this.GenToStringBtn);
+            this.Controls.Add(this.EncryptBtn);
+            this.Controls.Add(this.GenPvarBtn);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.PvarNameBox);
+            this.Controls.Add(this.EncryptNumBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BrowseBtn);
+            this.Controls.Add(this.FileInputBox);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "SQF Encrypt - By Shix";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox FileInputBox;
+        private System.Windows.Forms.Button BrowseBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox EncryptNumBox;
+        private System.Windows.Forms.TextBox PvarNameBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox outputBox;
+        private System.Windows.Forms.Button GenPvarBtn;
+        private System.Windows.Forms.Button EncryptBtn;
+        private System.Windows.Forms.Button GenToStringBtn;
+        private System.Windows.Forms.CheckBox CopyToClipboardCheck;
+        private System.Windows.Forms.CheckBox stripComments;
+    }
+}
+
---
+++ SQFEncrypt/SQFEncrypt/Main.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SQFEncrypt
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+        //Global vars 
+        public static class GlobVars
+        {
+            public static string inputFile { get; set; }
+            public static int encryptNum { get; set; }
+            public static string pVarName { get; set; }
+            public static string fileRead { get; set; }
+            public static string asciiStr { get; set; }
+        }
+
+        //Browse Btn
+        private void BrowseBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "SQF Files(.sqf) | *.sqf";
+            fileDialog.FilterIndex = 1;
+            fileDialog.Multiselect = false;
+            DialogResult result = fileDialog.ShowDialog();
+
+            //Only try to open the file if the user selected a file and clicked ok to prevent System.IO exception
+            if (result == DialogResult.OK)
+            {
+                outputBox.Text = "";
+                String fileName = fileDialog.FileName;
+                GlobVars.inputFile = fileName;
+                FileInputBox.Text = fileName;
+                openFile(fileName);
+            }
+        }
+
+        //Encrypt Btn
+        private void EncryptBtn_Click(object sender, EventArgs e)
+        {
+            if (!(string.IsNullOrWhiteSpace(FileInputBox.Text)))
+            {
+                byte[] ascii = Encoding.ASCII.GetBytes(GlobVars.fileRead);
+                int asciiCount = Encoding.ASCII.GetByteCount(GlobVars.fileRead);
+                int count = 0;
+                GlobVars.asciiStr = "";
+                foreach (byte b in System.Text.Encoding.UTF8.GetBytes(GlobVars.fileRead.ToCharArray()))
+                {
+                    if (!(string.IsNullOrWhiteSpace(EncryptNumBox.Text)))
+                    {
+                        int numPlus = GlobVars.encryptNum + b;
+                        GlobVars.asciiStr = GlobVars.asciiStr + numPlus;
+                        if (count < asciiCount - 1)
+                        {
+                            GlobVars.asciiStr = GlobVars.asciiStr + ",";
+                        }
+                    }
+                    else
+                    {
+                        GlobVars.asciiStr = GlobVars.asciiStr + b.ToString();
+                        if (count < asciiCount - 1)
+                        {
+                            GlobVars.asciiStr = GlobVars.asciiStr + ",";
+                        }
+                    }
+                    count = count + 1;
+                }
+                GlobVars.asciiStr = "_code = [" + GlobVars.asciiStr + "];";
+                outputBox.Text = GlobVars.asciiStr;
+            }
+            else
+            {
+                MessageBox.Show("Yeah... \nIt Might help if you had a file loaded!", "You Went full Retard", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            if (CopyToClipboardCheck.Checked)
+            {
+                Clipboard.SetText(GlobVars.asciiStr);
+            }
+        }
+
+        //Generate Pvar Btn
+        private void GeneratePVarbtn_Click(object sender, EventArgs e)
+        {
+            //make sure that the pvar name and encryptNum !Null
+            if (!(string.IsNullOrWhiteSpace(EncryptNumBox.Text) || string.IsNullOrWhiteSpace(PvarNameBox.Text)))
+            {
+                string pVarName = PvarNameBox.Text;
+                int pVarVale = GlobVars.encryptNum;
+                Clipboard.SetText(pVarName + " = " + pVarVale + ";\npublicVariable " + "''" + pVarName + "'';");
+                MessageBox.Show("Public Variable Code has been coppied to your clipboard", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Either your Encrypt Number or PublicVariableName is blank", "Pleb.exe", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        //Generate to string Btn
+        private void GenToStringBtn_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(PvarNameBox.Text))
+            {
+                string toString = "";
+                toString = toString + "_codeToRun = [];\n{\n";
+                toString = toString + "_num = _x - " + GlobVars.pVarName + ";\n";
+                toString = toString + "} forEach _code;\n";
+                toString = toString + "call compile toString _codeToRun;";
+                Clipboard.SetText(toString);
+                MessageBox.Show("toString Code has been coppied to your clipboard", "Sucess", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("You Need to set a public Variable name first", "You Went Full Retard", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void openFile(string fileName)
+        {
+            bool delNextLine = false;
+            string curLine;
+
+            System.IO.StreamReader file = new StreamReader(fileName);
+            while ((curLine = file.ReadLine()) != null)
+            {
+                if (stripComments.Checked)
+                {
+                    if (delNextLine == false)
+                    {
+                        if (curLine.Contains("/*"))
+                        {
+                            delNextLine = true;
+                        }
+                        else
+                        {
+                            if (!curLine.Contains("//"))
+                            {
+                                outputBox.AppendText("\n" + curLine);
+                                GlobVars.fileRead = GlobVars.fileRead + curLine;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (curLine.StartsWith("*/"))
+                        {
+                            delNextLine = false;
+                        }
+                    }
+                }
+                else
+                {
+                    outputBox.AppendText("\n" + curLine);
+                    GlobVars.fileRead = GlobVars.fileRead + curLine;
+                }
+            }
+        }
+
+        private void EncryptNumBox_TextChanged(object sender, EventArgs e)
+        {
+            //Throw an error if user is dumb enough to enter a char 
+            if (System.Text.RegularExpressions.Regex.IsMatch(EncryptNumBox.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Its called Encrypt Number. \nEnter a number you pleb not a char.", "You went full Retard", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                EncryptNumBox.Text = EncryptNumBox.Text.Substring(0, EncryptNumBox.Text.Length - 1);
+                EncryptNumBox.SelectionStart = EncryptNumBox.Text.Length;
+            }
+            else
+            {
+                if (!string.IsNullOrWhiteSpace(EncryptNumBox.Text))
+                {
+                    string EncryptNumS = EncryptNumBox.Text;
+                    int EncryptNum = int.Parse(EncryptNumS);
+                    GlobVars.encryptNum = EncryptNum;
+                }
+            }
+        }
+
+        private void PvarNameBox_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(PvarNameBox.Text))
+            {
+                GlobVars.pVarName = PvarNameBox.Text;
+            }
+            if ((PvarNameBox.Text.ToLower().Contains("gr8") || PvarNameBox.Text.ToLower().Contains("gg")))
+            {
+                MessageBox.Show("Fuck off Gr8 this is to stop people like you from stealing", "TWAT", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Close();
+            }
+        }
+
+        private void FileInputBox_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(FileInputBox.Text))
+            {
+                GlobVars.inputFile = FileInputBox.Text;
+            }
+        }
+    }
+}
